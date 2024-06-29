a4e50755531a847b08a5e9bad8826f00e532228f
DmitriyVolchenko <dmitriy.volchenko@gmail.com>
2024-06-29 17:31:44 +0300

    changes class MyArray to avoid O(N) complexity of clearing hashSet

--- src/telran/interviews/MyArray.java
+++ src/telran/interviews/MyArray.java
@@ -4,22 +4,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MyArray<T> {
-    private final Map<Integer, T> values;
+    private Map<Integer, T> values;
     private int size;
-    private int countForSetAll;
     private T valueForSetAll;
 
     public MyArray(int size) {
         this.size = size;
         values = new HashMap<>();
-        countForSetAll = -1;
         valueForSetAll = null;
     }
 
     public void setAll(T value) {
-        countForSetAll++;
         valueForSetAll = value;
-        values.clear();
+        values = new HashMap<>();
     }
 
     public void set(int index, T value) {
